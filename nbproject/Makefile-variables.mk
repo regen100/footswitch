1506daa8398734164c6d7b323519805026a6ab10
Regen <git@exadge.com>
2013-04-13 20:06:52 +0900

    Initial commit.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FootSwitch.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FootSwitch.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=footswitch.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/footswitch.tar
